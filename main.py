e486a763bdbe6bfe314e14f6b0e30fc58bcc3ea9
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-04-09 20:01:58 -0400

    StreamLit Integration

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediction App")
+
+stocks = ("AAPL", "GOOG", "AMZN", "MSFT", "GME", "NFLX")
+selected_stock = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction", 1, 4)
+period = n_years * 365
+
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stock)
+data_load_state.text("Loading data...done!")
