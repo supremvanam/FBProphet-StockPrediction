8049979e055c9c17bd200323e44b8e39605c97b2
Suprem Vanam <supremvanam.gsp@gmail.com>
2022-04-10 20:47:03 -0400

    Updated styles.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from datetime import date
+import styles
 
 import yfinance as yf
 from prophet import Prophet
@@ -9,12 +10,31 @@ from plotly import graph_objs as go
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-st.title("Stock Prediction App")
+st.set_page_config(
+     page_title="COMP 377 | Group 6",
+    #  layout="wide",   
+ )
+st.title("Predictr - Stock Prediction App")
+
+
+
+hide_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
+
+st.markdown(hide_style, unsafe_allow_html=True)
+st.markdown(styles.footer,unsafe_allow_html=True)
+
 
 stocks = ("AAPL", "GOOG", "AMZN", "MSFT", "GME", "NFLX")
-selected_stock = st.selectbox("Select dataset for prediction", stocks)
+# selected_stock = st.selectbox("Select dataset for prediction", stocks)
+selected_stock = st.text_input("Select dataset for prediction", placeholder="Example: AAPL")
 
-n_years = st.slider("Years of prediction", 1, 4)
+
+n_years = st.slider("Years of prediction", 1, 5)
 period = n_years * 365
 
 @st.cache
@@ -23,13 +43,6 @@ def load_data(ticker):
     data.reset_index(inplace=True)
     return data
 
-# data_load_state = st.text("Load data...")
-data = load_data(selected_stock)
-# data_load_state.text("Loading data...done!")
-
-st.subheader("Raw data")
-st.write(data.tail())
-
 def plot_raw_data():
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
@@ -37,25 +50,31 @@ def plot_raw_data():
     fig.layout.update(title_text = "Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
-plot_raw_data()
+if(selected_stock):
+    data = load_data(selected_stock)
+    st.subheader("Raw data")
+    st.write(data.tail())
+    plot_raw_data()
 
-# Forecasting
-df_train = data[['Date', 'Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+    # Forecasting
+    df_train = data[['Date', 'Close']]
+    df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+    with st.spinner(f'Predicting "{selected_stock}" stock for {n_years} year(s)'):
+        m = Prophet()
+        m.fit(df_train)
+        future = m.make_future_dataframe(periods=period)
+        forecast = m.predict(future)
 
-st.subheader('Forecast data')
-st.write(forecast.tail())
+        st.subheader('Forecast data')
+        st.write(forecast.tail())
 
 
-st.write('forecast data')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+        st.write('forecast data')
+        fig1 = plot_plotly(m, forecast)
+        st.plotly_chart(fig1)
 
-st.write('forecast components')
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+        st.write('forecast components')
+        fig2 = m.plot_components(forecast)
+        st.write(fig2)
+    st.success(f'Stock prediction for {selected_stock} is succesful')
---
+++ styles.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+footer="""<style>
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>COMP 377 | Group 6 <a style='display: block; text-align: center;' href="https://github.com/supremvanam/FBProphet-StockPrediction" target="_blank">Github</a></p>
+</div>
+"""
